83d2f49fe2638a8e11700efec7a40427e4fe5fd3
jiwon <leeji7031@gmail.com>
2023-11-24 12:21:43 +0900

    day06

---
+++ day8/08-01-vue-with-drf/vue-project/src/views/DetailView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Detail</h1>
+    <p>제목 : {{ article?.title }}</p>
+    <p>내용 : {{ article?.content }}</p>
+    <p>작성일 : {{ article?.created_at }}</p>
+    <p>수정일 : {{ article?.updated_at }}</p>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+const store = useCounterStore()
+const route = useRoute()
+const article = ref(null)
+
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: `${store.API_URL}/api/v1/articles/${route.params.id}`
+  })
+    .then((res) => {
+      article.value = res.data
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+</script>
+
+<style>
+
+</style>
